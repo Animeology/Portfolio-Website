0bac126375f449a2a28220c6bb76a28042651b5d
Animeology <joseph.nguyen19@yahoo.com>
2025-01-08 13:50:06 -0600

    Added picture to homepage

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Portfolio website loaded!');
+const img = document.getElementById("hover-image");
+
+img.addEventListener("mouseover", () => {
+  img.style.opacity = 0; // Fade out current image
+  setTimeout(() => {
+    img.src = "images/linkedin.png"; // Swap image after fading out
+    img.style.opacity = 1; // Fade in new image
+  }, 300); // Wait for the fade-out transition to finish
+});
+
+img.addEventListener("mouseout", () => {
+  img.style.opacity = 0; // Fade out current image
+  setTimeout(() => {
+    img.src = "images/github.png"; // Swap back to default image
+    img.style.opacity = 1; // Fade in original image
+  }, 300);
+});
